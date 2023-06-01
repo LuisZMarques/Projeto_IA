3b831b9528024d155f04feae60120027560cc3e7
-Luis <2212522@my.ipleiria.pt>
2023-06-01 18:33:15 +0100

    novo compute fitness

--- warehouse/warehouse_individual.py
+++ warehouse/warehouse_individual.py
@@ -11,15 +11,34 @@ class WarehouseIndividual(IntVectorIndividual):
     def __init__(self, problem: "WarehouseProblem", num_genes: int):
         super().__init__(problem, num_genes)
         self.forklifts_paths = []
-        self.lenPath = 0
-        self.penalizacao = 0
-        # TODO
-    def compute_fitness(self) -> float:
-        # TODO
-        self.path = [None] * len(self.problem.forklifts)
+        #self.biggest_path_steps = 0
+        self.gen_divided = []
         self.fitness = 0
+        self.penalizacao = 0
+        
 
-        # Definir o número de pontos de divisão
+    def compute_fitness(self) -> float:
+        self.fitness = 0
+        self.penalizacao = 0
+
+        forklifts_paths, big_path_steps = self.obtain_all_path()
+
+        for p in forklifts_paths:
+            self.fitness += len(p)
+       
+        for i in range(big_path_steps):
+            for j in range(len(forklifts_paths)):
+                if i < len(forklifts_paths[j]) and i < len(forklifts_paths[j-1]) and forklifts_paths[j][i] == forklifts_paths[j-1][i]:
+                    self.penalizacao += 1
+                    print("Penalização: "+ str(self.penalizacao))
+                    print(str(forklifts_paths[j][i]) + " " + str(forklifts_paths[j-1][i]))
+
+        return self.fitness+self.penalizacao
+
+    def obtain_all_path(self):
+        self.path = [None] * len(self.problem.forklifts)
+        self.biggest_path_steps = 0
+
         num_pontos_divisao = len(self.problem.forklifts)-1
 
         # Gerar pontos de divisão aleatórios
@@ -31,41 +50,9 @@ class WarehouseIndividual(IntVectorIndividual):
         # Dividir o array em partes desiguais com os pontos de divisão aleatórios
         new = np.split(self.genome, pontos_divisao)
 
-        for f in range(len(self.problem.forklifts)):
-
-            # do forklift ate ao 1º produto
-            for i in self.problem.pairs:
-                if i.cell1 == self.problem.forklifts[f] and i.cell2 == self.problem.products[new[f][0]]:
-                    self.fitness += i.value
-
-            for j in range(len(new[f]) - 1):
-                for k in self.problem.pairs:
-                    if k.cell1 == self.problem.products[new[f][j + 1]] and k.cell2 == self.problem.products[new[f][j]]:
-                        self.fitness += k.value
-                    elif k.cell1 == self.problem.products[new[f][j]] and k.cell2 == self.problem.products[
-                        new[f][j + 1]]:
-                        self.fitness += k.value
-
-            # do ultimo produto ate à exit
-            for l in self.problem.pairs:
-                if l.cell1 == self.problem.products[new[f][len(new[f]) - 1]] and l.cell2 == self.problem.exit:
-                    self.fitness += l.value
-
-
-        return self.fitness+self.penalizacao
-
-    def obtain_all_path(self):
-        # TODOk
-
-        self.path = [None] * len(self.problem.forklifts)
-        self.lenPath = 0
-
-        new = np.array_split(self.genome, len(self.problem.forklifts))
-
         # do forklift ate ao 1º produto
         for f in range(len(self.problem.forklifts)):
             self.path[f] = []
-            #print("A forklift " + str(f) + " vai apanhar os produtos : " + str(new[f]))
             for i in self.problem.pairs:
                 if i.cell1 == self.problem.forklifts[f] and i.cell2 == self.problem.products[new[f][0]]:
                     for j in i.path:
@@ -87,20 +74,20 @@ class WarehouseIndividual(IntVectorIndividual):
                         self.path[f].append(d)
 
             self.forklifts_paths.insert(f, self.path[f])
-            if self.lenPath < len(self.path[f]):
-                self.lenPath = len(self.path[f])
+            if self.biggest_path_steps < len(self.path[f]):
+                self.biggest_path_steps = len(self.path[f])
 
-        #print("N de forklifts : " + str(len(self.forklifts_paths)))
-        # print("N de caminhos : " + str(len(self.path)))
+        self.gen_divided.clear()
+        self.gen_divided = new
 
-        #print("steps Obtain : " + str(self.lenPath))
-
-        return [self.forklifts_paths, self.lenPath]
+        return [self.forklifts_paths, self.biggest_path_steps]
 
     def __str__(self):
         string = 'Fitness: ' + f'{self.fitness}' + '\n'
-        string += str(self.genome) + "\n\n"
-        # TODO
+        string += 'Genoma: ' + str(self.genome) + "\n"
+        string += 'Penalização: ' + f'{self.penalizacao}' + "\n\n"
+        for i in range(len(self.gen_divided)):
+            string += 'Forklift ' + str(i) + ': ' + str(self.gen_divided[i]) + "\n"
         return string
 
     def better_than(self, other: "WarehouseIndividual") -> bool:
@@ -111,5 +98,8 @@ class WarehouseIndividual(IntVectorIndividual):
         new_instance = self.__class__(self.problem, self.num_genes)
         new_instance.genome = self.genome.copy()
         new_instance.fitness = self.fitness
+        new_instance.penalizacao = self.penalizacao
+        for i in range(len(self.gen_divided)):
+            new_instance.gen_divided.insert(i, self.gen_divided[i].copy())
         # TODO
         return new_instance
